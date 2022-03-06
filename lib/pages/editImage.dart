4e59b70884f313b178cf1af97ef527a56fab3e60
Rangoiv <rangoiv@gmail.com>
2022-03-06 22:39:21 +0100

    Added basic image scanning and filtering.

---
+++ lib/pages/editImage.dart
@@ -0,0 +1,37 @@
+// Used for photo filters:
+// https://pub.dev/packages/photofilters
+// Image to PDF:
+// https://camposha.info/flutter/flutter-image-to-pdf/#gsc.tab=0
+
+
+import 'dart:io';
+
+import 'package:image/image.dart' as imageLib;
+import 'package:photofilters/filters/image_filters.dart';
+import 'package:photofilters/filters/subfilters.dart';
+//import 'package:photofilters/utils/convolution_kernels.dart';
+
+//import 'package:penzz/constants.dart';
+
+Future<File> editImage(File imageFile) async {
+  // Decode the image from file
+  var photo = imageLib.decodeImage(await imageFile.readAsBytes())!;
+
+  // Apply filters
+  var customImageFilter = ImageFilter(name: "Custom Image Filter");
+
+  customImageFilter.addSubFilter(ContrastSubFilter(0.3));
+  customImageFilter.addSubFilter(SaturationSubFilter(-0.7));
+
+  //var gaussianKernel = ConvolutionKernel([1,2,1,2,4,2,1,2,1], bias: 0);
+  //customImageFilter.addSubFilter(ConvolutionSubFilter.fromKernel(gaussianKernel));
+  //customImageFilter.addSubFilter(ConvolutionSubFilter.fromKernel(sharpenKernel));
+
+  customImageFilter.apply(photo.getBytes(), photo.width, photo.height);
+
+  // Write image to file
+  var list = imageLib.writePng(photo);
+  await imageFile.writeAsBytes(list);
+
+  return imageFile;
+}
--- lib/pages/takingPicturePage.dart
+++ lib/pages/takingPicturePage.dart
@@ -1,30 +1,13 @@
+// Code used:
+// https://docs.flutter.dev/cookbook/plugins/picture-using-camera
+
 import 'dart:async';
 import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-Future<void> main() async {
-  // Ensure that plugin services are initialized so that `availableCameras()`
-  // can be called before `runApp()`
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // Obtain a list of the available cameras on the device.
-  final cameras = await availableCameras();
-
-  // Get a specific camera from the list of available cameras.
-  final firstCamera = cameras.first;
-
-  runApp(
-    MaterialApp(
-      theme: ThemeData.dark(),
-      home: TakePictureScreen(
-        // Pass the appropriate camera to the TakePictureScreen widget.
-        camera: firstCamera,
-      ),
-    ),
-  );
-}
+import 'editImage.dart';
 
 // A screen that allows users to take a picture using a given camera.
 class TakePictureScreen extends StatefulWidget {
@@ -97,6 +80,7 @@ class TakePictureScreenState extends State<TakePictureScreen> {
             // Attempt to take a picture and get the file `image`
             // where it was saved.
             final image = await _controller.takePicture();
+            File editedImage = await(editImage(File(image.path)));
 
             // If the picture was taken, display it on a new screen.
             await Navigator.of(context).push(
@@ -104,7 +88,7 @@ class TakePictureScreenState extends State<TakePictureScreen> {
                 builder: (context) => DisplayPictureScreen(
                   // Pass the automatically generated path to
                   // the DisplayPictureScreen widget.
-                  imagePath: image.path,
+                  imagePath: editedImage.path,
                 ),
               ),
             );
