9e4e9744e60b20a81901b7c470d623a5c64d6999
Boltzman314 <sven.barac@gmail.com>
2022-05-07 22:13:52 +0200

    Added registration screen

--- lib/pages/registration_screen.dart
+++ lib/pages/registration_screen.dart
@@ -3,6 +3,7 @@ import 'package:penzz/helpers/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:penzz/pages/welcome_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:penzz/widgets/background.dart';
 
 
 class RegistrationScreen extends StatefulWidget {
@@ -21,42 +22,87 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20.0),
+      resizeToAvoidBottomInset: false,
+      body: Background(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            Container(
+              padding: const EdgeInsets.only(left: 15, right: 12, top: 40),
+              margin: EdgeInsets.only(bottom: 30),
+              child: Text('Briga o tvom zdravlju počinje ovdje', style: TextStyle(fontSize: 40, fontWeight: FontWeight.w900, fontFamily: 'Poppins-SemiBold')),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 30, left: 30, right: 30),
+              child: Container(
+                child: Center(child: Image.asset('images/penzzTextWhite.png', width: 220,),),
+                height: 150,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.teal.withOpacity(0.98),
+                      Colors.greenAccent.withOpacity(0.98),
+                    ],
+                    begin: Alignment.bottomLeft,
+                    end: Alignment.centerRight,
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(10, 10),
+                        blurRadius: 20,
+                        color: Colors.teal.withOpacity(0.2)),
+                  ],
+                ),
+              ),
+            ),
 
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (value) {
-                email = value;
-              },
-              decoration: kTextFieldDecoration.copyWith(hintText: 'Enter your email')
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              obscureText: true,
-              onChanged: (value) {
-                password = value;
-              },
-              decoration: kTextFieldDecoration.copyWith(hintText: 'Enter your password')
+
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Material(
+                color: Colors.black,
+                borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                elevation: 1,
+
+                child: TextField(
+                  style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700),
+                  keyboardType: TextInputType.emailAddress,
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  decoration: kTextFieldDecoration.copyWith(hintText: 'E-mail', hintStyle: TextStyle(color: Colors.white, fontWeight: FontWeight.w900,  fontFamily: 'Poppins-SemiBold')),
+                  textAlign: TextAlign.center,
+                ),
+              ),
             ),
             SizedBox(
               height: 24.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
               child: Material(
-                color: Colors.blueAccent,
+                color: Colors.black,
                 borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                elevation: 5.0,
+                elevation: 1,
+                child: TextField(
+                  style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700),
+
+                  obscureText: true,
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  decoration: kTextFieldDecoration.copyWith(hintText: 'Lozinka',hintStyle: TextStyle(color: Colors.white, fontWeight: FontWeight.w900,  fontFamily: 'Poppins-SemiBold'),),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 110),
+              child: Material(
+                color: Colors.black,
+                borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                elevation: 1.0,
                 child: MaterialButton(
                   onPressed:() async {
                     try {
@@ -75,11 +121,13 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                   height: 42.0,
                   child: Text(
                     'Stvorite račun',
-                    style: TextStyle(color: Colors.white),
+                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.w900,  fontFamily: 'Poppins-SemiBold'),
                   ),
                 ),
               ),
             ),
+            Padding(padding: EdgeInsets.only(top: 10),child: Image.asset('images/penzzTextBlack.png', height: 25,),)
+
           ],
         ),
       ),
